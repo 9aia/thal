1837ad7236cb472d362c2bd2cbff77b5230dc14e
Luis Float <me@luisfloat.com>
2024-01-25 14:07:12 -0300

    chore: config file nesting patterns

--- src/auth/pages/index/+Page.vue
+++ src/auth/pages/index/+Page.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import Btn from "#design/components/action/Btn.vue";
+import GoogleLogo from "#design/components/display/GoogleLogo.vue";
+</script>
+
 <template>
-  <h1>Auth</h1>
+  <div class="mx-auto max-w-lg">
+    <h1>Auth</h1>
+
+    <form action="/api/auth/google" method="GET">
+      <Btn
+        type="submit"
+        class="btn mt-2 w-full flex gap-1"
+      >
+        <GoogleLogo />
+        Continuar com Google
+      </Btn>
+    </form>
+  </div>
 </template>
