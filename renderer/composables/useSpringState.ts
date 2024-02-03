824f661f9ebd4f3fe5e1c4cf98a788dee8152906
Luis Float <me@luisfloat.com>
2024-02-02 22:25:20 -0300

    build: add reactivity utils

---
+++ renderer/composables/useSpringState.ts
@@ -0,0 +1,28 @@
+import { onUnmounted, ref, Ref, UnwrapRef } from "vue";
+
+function useSpringState<V>(value: UnwrapRef<V>) {
+  const state: Ref<UnwrapRef<V>> = ref(value as any);
+  const isPending: Ref<boolean> = ref(false);
+
+  let timer: ReturnType<typeof setTimeout>;
+
+  const set = (temporaryValue: UnwrapRef<V>, delay: number) => {
+    state.value = temporaryValue;
+    isPending.value = true;
+
+    timer = setTimeout(() => {
+      state.value = value;
+      isPending.value = false;
+    }, delay);
+  };
+
+  const abort = () => {
+    clearTimeout(timer);
+  };
+
+  onUnmounted(abort);
+
+  return { state, set, abort, isPending };
+}
+
+export default useSpringState;
