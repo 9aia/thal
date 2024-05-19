9d64de342122c2763b77ff247872146a25c94641
vinicenter <viniemidiobosi@gmail.com>
2024-05-19 00:37:22 -0300

    wip: add routes for lesson

---
+++ app/pages/learn/unit/[unitId]/level/[levelId].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    unitId: {{ route.params.unitId }}
+    levelId: {{ route.params.levelId }}
+  </div>
+</template>
