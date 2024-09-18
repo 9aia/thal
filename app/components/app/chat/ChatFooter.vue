3c5c0a24defc4d02d54ca9035b668b9ae4549286
vinicenter <viniemidiobosi@gmail.com>
2024-09-17 22:36:00 -0300

    wip: fix message sending with enter

--- app/components/app/chat/ChatFooter.vue
+++ app/components/app/chat/ChatFooter.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { useMagicKeys } from "@vueuse/core"
+
 const emit = defineEmits<{
   (e: "send"): void
 }>()
@@ -7,16 +9,32 @@ const text = defineModel<string>({
   required: true,
 })
 
+const {
+  shift,
+} = useMagicKeys()
+
 const isEmpty = computed(() => !text.value.trim())
 const icon = computed(() => {
   return isEmpty.value ? "mic" : "send"
 })
+
+function handleEnter(e: Event) {
+  if (!text.value.trim()) {
+    e.preventDefault()
+    return
+  }
+
+  if (!shift.value) {
+    e.preventDefault()
+    emit("send")
+  }
+}
 </script>
 
 <template>
   <footer class="px-3 py-2 bg-white flex items-end justify-center gap-2">
     <div class="flex-1 flex">
-      <ContentEditable is="span" v-model="text" class="input w-full h-auto p-[10px] textarea" placeholder="Type a message" />
+      <ContentEditable is="span" v-model="text" class="input w-full h-auto p-[10px] textarea" placeholder="Type a message" @keydown.enter="handleEnter" />
     </div>
 
     <div v-if="!isEmpty" role="button" class="btn btn-ghost btn-circle avatar" @click="emit('send')">
