bb1e5964f78d3938499449551228efff6b6dbeeb
vinicenter <viniemidiobosi@gmail.com>
2025-01-04 01:09:48 -0300

    fix: navbar height
    Co-authored-by: Luis Emidio <luisfuturist@gmail.com>

--- app/components/ui/navigation/Navbar.vue
+++ app/components/ui/navigation/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-3 py-2 bg-slate-800 flex gap-2 justify-between">
+  <div class="px-3 py-2 h-[64px] bg-slate-800 flex gap-2 justify-between">
     <slot />
   </div>
 </template>
