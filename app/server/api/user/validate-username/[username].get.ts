f8464e460e67e26862adc23ff8cc832671534840
vinicenter <viniemidiobosi@gmail.com>
2025-02-16 22:06:37 -0300

    fix: use authentication for validate user route

--- app/server/api/user/validate-username/[username].get.ts
+++ app/server/api/user/validate-username/[username].get.ts
@@ -2,9 +2,14 @@ import { eq } from 'drizzle-orm'
 import { z } from 'zod'
 import { usernameSchema, users } from '~~/db/schema'
 import { getValidated } from '~/utils/h3'
+import { unauthorized } from '~/utils/nuxt'
 
 export default eventHandler(async (event) => {
   const orm = event.context.orm
+  const user = event.context.user
+
+  if (!user)
+    throw unauthorized()
 
   const { username } = await getValidated(event, 'params', z.object({ username: z.string() }))
 
