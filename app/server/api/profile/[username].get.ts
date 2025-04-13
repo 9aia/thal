44875cd496bc2c418489c0ce6932f6dd4e7c2385
vinicenter <viniemidiobosi@gmail.com>
2025-04-13 19:14:04 +0000

    refactor: enhance profile API with error handling for invalid and non-existing usernames
    Co-authored-by: Luis Emidio <luisfuturist@gmail.com>

--- app/server/api/profile/[username].get.ts
+++ app/server/api/profile/[username].get.ts
@@ -1,27 +1,29 @@
 import { eq } from 'drizzle-orm'
 import { z } from 'zod'
 import { getValidated } from '~/utils/h3'
-import { notFound, unauthorized } from '~/utils/nuxt'
-import { users } from '~~/db/schema'
+import { forbidden, notFound, unauthorized } from '~/utils/nuxt'
+import { usernameSchema, users } from '~~/db/schema'
 
 export default eventHandler(async (event) => {
-  const { username } = await getValidated(event, 'params', z.object({ username: z.string() }))
-
   const orm = event.context.orm
   const user = event.context.user
+
+  const { username } = await getValidated(event, 'params', z.object({
+    username: usernameSchema,
+  }))
 
   if (!user)
     throw unauthorized()
 
-  const profile = (
-    await orm.select().from(users).where(eq(users.username, username))
-  ).at(0)
+  if (user.username !== username)
+    throw forbidden(`You are not allowed to view this profile: ${username}`)
+
+  const [profile] = await orm.select()
+    .from(users)
+    .where(eq(users.username, username))
 
   if (!profile)
     throw notFound(`User not found: ${username}`)
-
-  // if (user.username !== username)
-  //   throw forbidden(`You are not allowed to view this profile: ${username}`)
 
   return profile
 })
